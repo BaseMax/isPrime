c63928507209367a49f316da38e32b7723adc8c2
Max Base <MaxBaseCode@Gmail.Com>
2019-04-25 15:30:36 +0430

    Update Check.cpp

--- Check.cpp
+++ Check.cpp
@@ -15,6 +15,19 @@ using std::cin;
 using std::cout;
 
 bool isPrime(int number) {
+	int con=0;
+	for(int g=2; g<number; g++) {
+		if(number%g == 0) {
+			con++;
+		}
+	}
+	if(con == 0) {
+		return true;
+	}
+	return false;
+}
+
+bool isPrimeBetter(int number) {
 	if(number <= 1)
 		return false;
 	if(number % 2 == 0 && number > 2)
@@ -54,7 +67,8 @@ int main() {
 	int number;
 	cin >> number;
 	for(int index=1; index<=number; index++) {
-		if(isPrime(index)) {
+		// if(isPrime(index)) {
+		if(isPrimeBetter(index)) {
 			cout << index << "\n";
 		}
 	}
