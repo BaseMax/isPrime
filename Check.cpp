b9cd285ffb52f6f59a22627d3b8b823cbf9a5162
Max Base <MaxBaseCode@Gmail.Com>
2019-04-25 15:28:49 +0430

    Update Check.cpp

--- Check.cpp
+++ Check.cpp
@@ -9,7 +9,34 @@
 *
 **/
 #include <iostream>
-using namespace std;
+#include <iostream>
+// using namespace std;
+using std::cin;
+using std::cout;
+
+bool isPrime(int number) {
+	if(number <= 1)
+		return false;
+	if(number % 2 == 0 && number > 2)
+		return false;
+	for(int i = 3; i < number / 2; i+= 2) {
+		if(number % i == 0)
+			return false;
+	}
+	return true;
+}
+
+int main() {
+	int number;
+	cin >> number;
+	for(int index=1; index<=number; index++) {
+		if(isPrime(index)) {
+			cout << index << "\n";
+		}
+	}
+	return 0;
+}
+
 
 bool isPrime(int number) {
 	if(number <= 1)
