0c06c5628d5448ff3d94fb89a087bd8094e11326
Max Base <MaxBaseCode@Gmail.Com>
2019-04-25 15:27:08 +0430

    Create Check.cpp

---
+++ Check.cpp
@@ -0,0 +1,10 @@
+/**
+*
+* @Name : isPrime/Check.cpp
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-04-25
+* @Released under : https://github.com/BaseMax/isPrime/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/isPrime
+*
+**/
